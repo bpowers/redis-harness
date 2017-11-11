72e2219d48cf329104a22a3a525ed0ed2081b87c
Bobby Powers <bobbypowers@gmail.com>
2017-11-11 15:50:59 -0500

    lots of debug info about what mesh is doing

--- cmd.py
+++ cmd.py
@@ -3,6 +3,7 @@ import subprocess
 
 class Cmd(threading.Thread):
     def __init__(self, cmd, effect='stdout'):
+        # print('CMD: %s' % cmd)
         self.cmd = cmd
         self.effect = effect
         self.stdout = None
