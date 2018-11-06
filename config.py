7f3e6e3825803b9f73217762b9e131fe743fa3ec
Bobby Powers <bobbypowers@gmail.com>
2018-11-05 20:44:23 -0800

    test against all mesh configs

--- config.py
+++ config.py
@@ -11,11 +11,16 @@ class Config:
         self.defrag = defrag
 
 configs = [
-    Config('mesh-alwayson', 'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh'),
-    Config('mesh',          'USE_MESH=yes',             defrag=True),
+    Config('mesh0n',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh0n'),
+    Config('mesh1n',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh1n'),
+    Config('mesh2n',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh2n'),
+    Config('mesh0y',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh0y'),
+    Config('mesh1y',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh1y'),
+    Config('mesh2y',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh2y'),
+    Config('mesh-explicit', 'USE_MESH=yes',             defrag=True),
     Config('libc',          'USE_JEMALLOC=no'),
     Config('jemalloc',      'USE_JEMALLOC=yes',         defrag=True),
     Config('tcmalloc',      'USE_TCMALLOC_MINIMAL=yes'),
-    Config('hoard',         'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lhoard -L/usr/local/lib'),
-    Config('diehard',       'USE_JEMALLOC=no',          '-Wl,--no-as-needed -ldiehard -L/usr/local/lib'),
+    # Config('hoard',         'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lhoard -L/usr/local/lib'),
+    # Config('diehard',       'USE_JEMALLOC=no',          '-Wl,--no-as-needed -ldiehard -L/usr/local/lib'),
 ]
